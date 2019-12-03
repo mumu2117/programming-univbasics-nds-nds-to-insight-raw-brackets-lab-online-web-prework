79b1ad0116f2c69832ddb995a44a755ce79037b7
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-03 20:03:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,11 +12,19 @@ def directors_totals(nds)
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
+  
+  while name_index < nds.length do 
+    name = nds[name_index][:name]
+    result[name] = 0
+    movie_index = 0
+    #  this while loop hsould get the movie gross
+    #  
+    while movie_index < nds[name_index][:movies].length do 
+      result[name] += nds[name_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    name_index += 1
+  end
   # Be sure to return the result at the end!
   nil
 end
